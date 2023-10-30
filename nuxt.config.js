a2f1f7a10f4d4cdec53ee07594818d424f8795a4
sp <patel.l.sanjay12188@gmail.com>
2023-10-31 00:57:13 +0530

    - setup new template

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,9 @@ export default defineNuxtConfig({
       script: [
         {type:'module',src:'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js'},
         {src:'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js'},
-        {type:'module',src:'_nuxt/assets/js/uikit.min.js'},
-        {src:'_nuxt/assets/js/simplebar.js'},
-        {src:'_nuxt/assets/js/script.js'},
+        {type:'module',src:'https://demo.foxthemes.net/socialite-v3.0/assets/js/uikit.min.js'},
+        {src:'https://demo.foxthemes.net/socialite-v3.0/assets/js/simplebar.js'},
+        {src:'https://demo.foxthemes.net/socialite-v3.0/assets/js/script.js'},
 
       ],
     },
